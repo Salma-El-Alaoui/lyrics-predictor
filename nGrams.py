ef503636644e53f194174cf164530d2bbb4df0b4
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2015-10-08 16:19:30 +0200

    random sentence generation with basic ngram

--- nGrams.py
+++ nGrams.py
@@ -3,8 +3,7 @@ __author__ = 'qrr'
 import nltk
 from nltk.corpus import gutenberg
 from nltk.probability import *
-
-from random import randint
+import random
 
 
 blakePoems = gutenberg.words('blake-poems.txt')
@@ -20,22 +19,40 @@ def ngram_freq_cond(corpus, n):
                 for i in range(n-1, size) )
         return cd
 
-def generate_model(cfdist, context, num=15):
-    seed = randint(0,9)
-    n = len(context)
-    for j in range(n):
+def generate_sentence(corpus, n , length = 15):
+
+    fdist = ngram_freq_dist(corpus, n)
+    cfdist = ngram_freq_cond(corpus, n)
+
+    countStart = 0
+    listContext = []
+    for ngram in fdist :
+            if ngram[0] == '.' :
+                countStart += 1
+                listContext.append(ngram)
+
+    seed = random.randrange(0,countStart)
+    context = listContext[seed]
+    #remove the point
+    context = context[1:n]
+    for j in range(n-1):
             print(context[j], end = ' ')
-    for i in range(num):
+    for i in range(length):
         nextWord = cfdist[context].max()
         print(nextWord, end = ' ')
-        context = context[1:n]+(nextWord,)
+        if (nextWord == '.')|(nextWord == '!')|(nextWord == '?')| (((nextWord == ';')|(nextWord == ',')) & (i > length - 3)):
+            break
+        context = context[1:n-1]+(nextWord,)
 
-bigramCfd = ngram_freq_cond(blakePoems, 2)
-trigramCfd = ngram_freq_cond(blakePoems, 3)
-generate_model(bigramCfd, ('I',))
+
+generate_sentence(blakePoems, 2)
 print("\n")
-generate_model(trigramCfd, ('I','am'))
-generate_model()
+generate_sentence(blakePoems, 3)
+print("\n")
+generate_sentence(blakePoems, 4)
+print("\n")
+generate_sentence(blakePoems, 5)
+
 
 
 
