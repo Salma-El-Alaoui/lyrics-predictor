50d38d25fc54c65458d48494ba465481b76e27a8
truthspy <qirongrong0831@163.com>
2015-10-08 17:36:29 +0200

    nGramTag Modified for Python3

--- nGramTag.py
+++ nGramTag.py
@@ -7,6 +7,8 @@ brown_tagged_sents = brown.tagged_sents(categories='news')
 # print brown_tagged_sents
 brown_sents = brown.sents(categories='news')
 
+# print (list(brown_sents))
+
 size = int(len(brown_tagged_sents) * 0.8)
 
 train_sents = brown_tagged_sents[:size]
@@ -21,7 +23,7 @@ t3 = nltk.TrigramTagger(train_sents, backoff=t2)
 # print t2.tag(unseen_sents)
 
 for word in unseen_sents:
-    print t3.tag(word)
+    print (t3.tag(word))
 
-print t2.evaluate(test_sents)
-print t3.evaluate(test_sents)
+print (t2.evaluate(test_sents))
+print (t3.evaluate(test_sents))
