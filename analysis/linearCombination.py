e244ca64639521cf5a4e7b8b8d1f42136c467334
truthspy <qirongrong0831@163.com>
2015-10-13 20:47:04 +0200

    load corpus works

--- analysis/linearCombination.py
+++ analysis/linearCombination.py
@@ -2,10 +2,19 @@ __author__ = 'Salma'
 
 #from nGram import NgramTagModel,nGramModel
 import corpus.lyric_corpus.corpus_access as corpus
+from nGram.NgramTagModel import NgramTagModel
 
-train, test = corpus.loadCorpus()
+trainCorpus, testCorpus, devCorpus = corpus.loadCorpus()
 
-print(list(test))
+# print (testCorpus)
+
+tm = NgramTagModel(3,2,trainCorpus)
+testTag = tm.tagTestCorpus(testCorpus)
+context = tm.getRandomContext(testTag)
+print("Context", context)
+print(tm.nextWord(context))
+
+# print(sents)
 
 
 
